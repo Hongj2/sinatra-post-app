606a715bcc100d778a355eb5bcb4f246cd351226
Jessica <Jessicahong.nrnr@gmail.com>
2019-08-31 14:23:13 -0400

    updated user controller with routes and built welcome page, login/success/failure page, and sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,39 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+		set :session_secret, "password_security"
   end
 
   get "/" do
-    erb :login
+    erb :welcome
   end
 
+  get "/success" do
+		 if logged_in?
+			erb :success
+		 else
+		 	redirect "/failure"
+		 end
+	end
 
+  get "/failure" do
+		erb :failure
+	end
 
+  helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
 
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
+
+  get "/logout" do
+		session.clear
+		redirect "/"
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,41 @@ class UsersController < ApplicationController
     get '/login' do
         erb :login
     end
+    
+    post "/login" do
+        user = User.find_by(:username => params[:username])
+         
+          if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/success"
+          else
+            redirect "/failure"
+          end
+            end
+  
 
-    get '/signup' do
+    get "/signup" do
+		erb :signup
+	end
+# renders a form to create a new user. The form includes fields for username and password.
+
+	post "/signup" do
+	user = User.new(:username => params[:username], :password => params[:password])
+	#make a new instance of our user class with a username and password from params. Note that even though our database has a #column called password_digest, we still access the attribute of password. This is given to us by has_secure_password
+	 if user.save
+    redirect "/login"
+  else
+    redirect "/failure"
+  end
+  
     end
+
+
+
+    
+
 end
 
   #purpose is to render the log in form
 #no Base bc we want methods to be available to all other controllers 
+# the purpose fo post 'login' is to recieve the login form to find user and log user in 
